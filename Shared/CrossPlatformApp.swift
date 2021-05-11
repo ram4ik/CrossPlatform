500d76dda35311d53febe3296237b521ba0ae9be
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-11 22:32:27 +0300

    Initial Commit

---
+++ Shared/CrossPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossPlatformApp.swift
+//  Shared
+//
+//  Created by Ramill Ibragimov on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CrossPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
